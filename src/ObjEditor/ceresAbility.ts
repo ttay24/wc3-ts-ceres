5bfb9e756fc827bcfb7bac8a83e7855939de78c4
ttay24 <tanner.taylor24@gmail.com>
2020-02-02 22:42:49 -0600

    Adding Ceres ability class

---
+++ src/ObjEditor/ceresAbility.ts
@@ -0,0 +1,14 @@
+import { BaseObject } from './baseObject';
+
+export class CeresAbility extends BaseObject {
+
+  /**
+   * Expose the object (unit data) so its fields can be set
+   */
+  get a(): WarObjects { return this.obj; }
+
+  constructor(abilityId: string, baseAbilityId: string) {
+    super(abilityId, baseAbilityId);
+  }
+
+}
